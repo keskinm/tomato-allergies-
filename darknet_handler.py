78d670264da01523efebd7b5cf2156995af97266
keskin <keskin.mustafa.42@gmail.com>
2019-11-23 12:38:31 +0100

    Automate creation of sets pointer

--- darknet_handler.py
+++ darknet_handler.py
@@ -4,6 +4,17 @@ import shutil
 import argparse
 
 
+def create_sets_pointer_file():
+    sets_pointer_opened_file = open('./cfg/tomato.data', "w")
+    content = 'classes= 1\n' \
+              'train  = {wd}/data/formated/train.txt\n' \
+              'valid  = {wd}/data/formated/test.txt\n' \
+              'names = cfg/tomato.names\n' \
+              'backup = backup'.format(wd=os.getcwd())
+    sets_pointer_opened_file.write(content)
+    sets_pointer_opened_file.close()
+
+
 def main(install):
     if install:
         commands = ['wget https://github.com/AlexeyAB/darknet/archive/master.zip',
@@ -13,7 +24,7 @@ def main(install):
         os.remove('master.zip')
         darknet_dir = './darknet-master'
         subprocess.run('make', check=False, shell=True, cwd=darknet_dir)
-
+        create_sets_pointer_file()
         darknet_cfg_dir = '{}/cfg'.format(darknet_dir)
         shutil.copy('./cfg/tomato.data', darknet_cfg_dir)
         shutil.copy('./cfg/tomato.cfg', darknet_cfg_dir)
